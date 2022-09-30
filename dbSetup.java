bf1190eaa2ab2873c421f928d874070ac0143bf3
djohnson135 <Dathanljohnson135@gmail.com>
2022-09-30 09:01:25 -0500

    Starter code

---
+++ dbSetup.java
@@ -0,0 +1,6 @@
+public final class dbSetup  {
+  //TODO: Update user and pswd
+  public static final String user = "csce315_915_johnson";
+  public static final String pswd = "Dukenduchess128523";
+}
+
